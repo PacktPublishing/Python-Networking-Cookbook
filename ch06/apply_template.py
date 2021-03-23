2c2f215d2788265b90ad34e3a4430d261e9da3da
Marcel Neidinger <marcel.neidinger@nlogn.org>
2021-03-24 00:23:25 +0100

    ch06 correct template code

--- ch06/apply_template.py
+++ ch06/apply_template.py
@@ -1,4 +1,24 @@
 import napalm
+from jinja2 import Environment, FileSystemLoader
+
+# Generate template
+loader = FileSystemLoader("templates")
+environment = Environment(loader=loader)
+
+tpl = environment.get_template("acl.conf.tpl")
+
+allowed = [
+	"10.10.0.10",
+	"10.10.0.11",
+	"10.10.0.12"
+]
+disallowed = [
+	"10.10.0.50",
+	"10.10.0.62"
+]
+intf = "ethernet0"
+
+out = tpl.render(allowed=allowed, disallowed=disallowed, intf=intf)
 
 driver = napalm.get_network_driver("ios")
 
@@ -13,7 +33,7 @@ conn_details = {
 
 device = driver(**conn_details)
 device.open()
-device.load_merge_candidate(config='hostname test\n')
+device.load_merge_candidate(config=out)
 print(device.compare_config())
 
 user_inp = input("Do you want to apply the changes?[y/n] ")
